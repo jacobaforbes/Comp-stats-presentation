3321f8e4215b1654db645d6bbcbd47bcd46fe060
Jacob Forbes <45201187+jacobaforbes@users.noreply.github.com>
2022-11-28 09:49:01 -0500

    Initialized repo

---
+++ Archive/presentation_sandbox.R
@@ -0,0 +1,6 @@
+# Load the data
+dat = read.csv("//Users//jacob//Library//CloudStorage//OneDrive-Personal//Documents//UTK 22-25//Research//Comp Stats presentation//friends_and_the_office.csv")
+
+# Show stuff
+str(dat)
+summary(dat)
